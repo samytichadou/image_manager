200980e3b92fc2c1e2f76b6073cf6762ddf90f61
samytichadou <samytichadou@gmail.com>
2023-01-19 17:12:57 +0100

    ui improvement

--- ui_lists.py
+++ ui_lists.py
@@ -13,37 +13,40 @@ def return_image_users(image):
 # Internal Images
 class IMGMNG_UL_internal_images_uilist(bpy.types.UIList):
 
+    show_internal : bpy.props.BoolProperty(name = "Show Internals", description = "Show internal images")
+
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, flt_flag) :
         row = layout.row(align = True)
 
-        # LOGO
         # internal files
         if item.source in {'VIEWER','GENERATED'} \
         or not item.filepath:
-            row.label(text="", icon="FILE_BACKUP")
+            row.label(text="", icon="DOT")
+            row.label(text="", icon="BLENDER")
+            row.label(text=item.name)
+
         # external files
         else:
+            # RESOURCES FOLDER
+            folderpath=fld.return_image_folder()
+            if os.path.dirname(bpy.path.abspath(item.filepath))==folderpath:
+                row.label(text="", icon="CHECKMARK")
+            else:
+                row.operator('imgmng.copy_image', text="", icon="FOLDER_REDIRECT").image=item.name
+
             row.label(text="", icon="DISK_DRIVE")
 
-        # COMMON
-        row.prop(item, "name", text="", emboss=False)
-        # row.label(text=str(return_image_users(item)))
-        # row.prop(item, "users", text="", emboss=False)
-        row.prop(item, "use_fake_user", text="")
+            # COMMON
+            row.prop(item, "name", text="", emboss=False)
+            # row.label(text=str(return_image_users(item)))
+            # row.prop(item, "users", text="", emboss=False)
+            row.prop(item, "use_fake_user", text="")
 
-        # PACK UNPACK
-        if item.packed_file:
-            row.operator('imgmng.unpack_image', text="", icon="PACKAGE").image=item.name
-        else:
-            row.operator('imgmng.pack_image', text="", icon="UGLYPACKAGE").image=item.name
-
-        # RESOURCES FOLDER
-        folderpath=fld.return_image_folder()
-        if os.path.dirname(bpy.path.abspath(item.filepath))==folderpath:
-            row.label(text="", icon="CHECKMARK")
-        else:
-            row.operator('imgmng.copy_image', text="", icon="FOLDER_REDIRECT").image=item.name
-
+            # PACK UNPACK
+            if item.packed_file:
+                row.operator('imgmng.unpack_image', text="", icon="PACKAGE").image=item.name
+            else:
+                row.operator('imgmng.pack_image', text="", icon="UGLYPACKAGE").image=item.name
 
 # External Images
 class IMGMNG_UL_external_images_uilist(bpy.types.UIList):
